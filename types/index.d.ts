0a94dca9c68e436ed88746aaf32ca1e1a22627c4
Davin Chang Juan <dawenci@gmail.com>
2020-03-08 17:37:16 +0800

    ignore types

--- types/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Type, MemCache, LruInitOptions, LfuInitOptions, FifoInitOptions } from './types';
-declare type InitOptions = LruInitOptions | LfuInitOptions | FifoInitOptions;
-export declare function createCache<K = any, V = any>(type: Type, options?: Partial<InitOptions>): MemCache<K, V>;
-export {};
